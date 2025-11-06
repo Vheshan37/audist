e1d91a01e29931780a9d97b050f8d3b57709a353
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-11-06 19:11:27 +0530

    Delete viewCaseWithID.js

--- server/src/controllers/case/viewCaseWithID.js
+++
@@ -1,8 +0,0 @@
-
-const caseDetails = (req ,res ) =>{
-    console.log("Case details endpoint hit");
-
-    res.status(200).json({ message: "Case details endpoint is under construction. 001" });
-}
-
-module.exports ={caseDetails}
