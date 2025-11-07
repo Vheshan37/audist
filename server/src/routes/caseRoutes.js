59bdd58b3308cfec8f553b0a9fd8ce2d47b75e2f
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-11-07 22:44:53 +0530

    Update caseRoutes.js

--- server/src/routes/caseRoutes.js
+++ server/src/routes/caseRoutes.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const {cases,caseswithDate,addCase,updateCase,updatecaseDate} = require("../controllers/case/getAllCase");
+const {cases,caseswithDate,addCase,updateCase,updatecaseDate,caseDetails} = require("../controllers/case/getAllCase");
 
 const router = express.Router();
 
 router.post("/getAll", cases);
 router.post("/add", addCase);
 router.post("/getdetaliswithdate", caseswithDate);
-router.post("/viewDetails", caseswithDate);
+router.post("/viewDetails", caseDetails);
 
 router.put("/updateDate", updatecaseDate);
-router.put("/updateDetails", updateCase);
+router.post("/updateDetails", updateCase);
 
 module.exports = router; 
