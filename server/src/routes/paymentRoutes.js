a067345358c2dcd479251909364f41d8f858080c
Supun Sulakshana <supunsulakshana2001@gmail.com>
2025-11-07 19:06:58 +0530

    Update paymentRoutes.js

--- server/src/routes/paymentRoutes.js
+++ server/src/routes/paymentRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const {payment} = require("../controllers/payment/viewPaymentWithID");
-const { caseWithCaseNumb } = require("../controllers/payment/getPayments");
+const { caseWithCaseNumb , addPayment } = require("../controllers/payment/getPayments");
 
 const router = express.Router();
 
 router.get("/getdetaliswithid", payment);
 
-router.post("/getcasewithcasenumb", caseWithCaseNumb);
+router.post("/getdetails", caseWithCaseNumb);
+router.post("/getpayment", addPayment);
 
 module.exports = router;
